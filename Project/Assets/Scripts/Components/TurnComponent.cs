0a8dc9f8e823a21a0fe14053e1923a90fd412c52
ToTo-Mo <bum4496@naver.com>
2021-08-04 18:21:04 +0900

    프로젝트 초기화

--- Project/Assets/Scripts/Components/TurnComponent.cs
+++
@@ -1,7 +0,0 @@
-using Unity.Entities;
-
-[GenerateAuthoringComponent]
-public struct TurnComponent : IComponentData
-{
-    public float turnSpeed;
-}
--- Project/Assets/Scripts/Weaphone/Gun.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public abstract class Weaphone
-{
-    private string name;
-    private float range;
-    private float recoil;
-    private float accuracy;
-}
